4d35b1794a90ef805014fd55717407aec4075796
Herve Tribouilloy <herve@digitalrisedorset.co.uk>
2025-05-18 12:24:23 +0100

    Removed previous version of oauth-express component

--- oauth-express-bak/src/types/express-request.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/express/index.d.ts
-import {KeystoneUser} from "../lib/keystone";
-
-declare module 'express-serve-static-core' {
-    interface Request {
-        user?: KeystoneUser;
-    }
-}
--- oauth-express-bak/src/types/express/index.d.ts
+++
@@ -1,7 +0,0 @@
-import {KeystoneUser} from "../../lib/keystone";
-
-declare global {
-    namespace Express {
-        interface User extends KeystoneUser {}
-    }
-}
